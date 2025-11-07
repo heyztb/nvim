9b77a0764107fa2b37e3bdc67d9cf3a690aa7a5c
Zach Blake <zblake@nrttech.com>
2025-11-06 20:37:28 -0800

    add everforest

--- lua/heyztb/plugins/colorscheme.lua
+++ lua/heyztb/plugins/colorscheme.lua
@@ -252,4 +252,15 @@ return {
 			-- })
 		end,
 	},
+	{ -- NOTE: everforest
+		"sainnhe/everforest",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.everforest_enable_italic = true
+			vim.g.everforest_transparent_background = 1
+		end,
+	},
 }
