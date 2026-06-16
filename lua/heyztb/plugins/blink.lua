c290e5d35be86c0a2a4240b2e21a49075a4ba684
Zachary Blake <hi@ztb.dev>
2026-06-15 20:51:44 -0700

    remove redundant supermaven file, now dep of blink.cmp

--- lua/heyztb/plugins/blink.lua
+++ lua/heyztb/plugins/blink.lua
@@ -8,7 +8,6 @@ return {
 			{
 				"supermaven-inc/supermaven-nvim",
 				opts = {
-					disable_inline_completion = true, -- disables inline completion for use with cmp
 					disable_keymaps = true, -- disables built in keymaps for more manual control
 				},
 			},
@@ -32,7 +31,7 @@ return {
 						auto_show = true,
 					},
 					ghost_text = {
-						enabled = false,
+						enabled = true,
 						show_with_menu = true,
 					},
 					accept = {
